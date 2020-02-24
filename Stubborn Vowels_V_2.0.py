b942b92d34377da4d5c9846a236999b496ac9eb9
Janani <52209332+Jananipalanivel@users.noreply.github.com>
2020-02-24 17:57:07 +0530

    Create  Stubborn Vowels_V_2.0.py

---
+++ Stubborn Vowels_V_2.0.py
@@ -0,0 +1,15 @@
+#question
+Given a string N and number of rotations K, rotate the fragments seperated by the vowels K times to the left hand side without bothering the postiion of the vowels.
+Input Format
+Frist line of input is a string input Second line of input is a integer input K, so rotate the string K times to the left side.
+Constraints
+3<=length of the string<=10^(10^7) 0<=K<=10^9
+Output Format
+Rotated string has to be printed
+Sample Input 0
+missing
+1
+Sample Output 0
+ssingim
+Explanation 0
+m + i + ss + i + ng all the fragments of string - {m,ss,ng} will be rotated once to the left - {ss,ng,m} OUTPUT: ssingim
