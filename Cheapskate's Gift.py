5f66917af1a403f8df5b1316356dc677f009c787
Janani <52209332+Jananipalanivel@users.noreply.github.com>
2020-02-25 08:59:03 +0530

    Create Cheapskate's Gift.py

---
+++ Cheapskate's Gift.py
@@ -0,0 +1,14 @@
+Rohit is a stingy person, he dosent like spending money. He had a friend by the name Kamal, and he invited Rohit for his daughter's birthday party. He was happy to go to the party but a sad that he had to spend some money to buy a gift for her. He didnt want to spend much in buyinng the gift. He knew that Kamal's daughter had a coin collection, so he thought he can give a coin which she didnt have and that too a least possible value coin as possible. But he didnt wanna look that much obviously stingy, so he decided not to give a coin of any denominatino that she has already and also the coin value must not be equal to sum of any coins she has already, but also least value coin as possible. So can you help Rohit to find the least value coin as possible.
+Note: Lets assume there are coins of all denominatinos
+Input Format
+First line of innput has one single integer N denoting the varoius denominations of coins Kamal's daughter has The second line of input has N spaced integer denoting each dominations.
+Constraints
+1<=N<=10^9 1<=a[i]<=10^9 N - numebr of denominations a[i] - Value of denomination
+Output Format
+Least possible Value of the coin
+Sample Input 0
+3
+1 4 5
+Sample Output 0
+2
+
