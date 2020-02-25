0fb1a0322607b0ebd34528a6b4f78125560c1b9c
Janani <52209332+Jananipalanivel@users.noreply.github.com>
2020-02-25 18:00:23 +0530

    Create  WORD OF THE GIVEN RANK.py

---
+++ WORD OF THE GIVEN RANK.py
@@ -0,0 +1,17 @@
+#question
+You are given a word (String) 'S' and an integer representing the rank 'r', Your task is to permute through the string and arrange them in increasing order and say the word that is present in the index of 'r'.
+Note: indexing starts from one
+Input Format
+Single line input contains a string 's' and an integer 'r' seprated by space
+Constraints
+1<=len(s)<=20
+Output Format
+single integer 'res'
+Sample Input 0
+CAB 2
+Sample Output 0
+ACB
+Explanation 0
+The possible permutations in lex order are,
+1 ABC 2 ACB 3 BAC 4 BCA 5 CAB 6 CBA
+The Word for rank 2 us ACB
