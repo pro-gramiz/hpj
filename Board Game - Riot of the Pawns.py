c19f5f58cd5358046321890d0c3231df3c50eb80
Janani <52209332+Jananipalanivel@users.noreply.github.com>
2020-02-26 08:52:17 +0530

    Create  Board Game - Riot of the Pawns.py

---
+++ Board Game - Riot of the Pawns.py
@@ -0,0 +1,18 @@
+#question
+Chess is a two-player strategy board game played on a checkered board with 64 squares arranged in an 8×8 grid. The game is played by millions of people worldwide. Pawn might take smaller steps but yet is considered to be one of the most powerfull coin. Since the pawns performed well in a particular war, they were given a promotion to bishop category. A Bishop is possible to move along any diagonal. "With greater power comes greater responsibilities" is a statement from the Spiderman movie, yet a line with great meaning. The Pawns wern't that much responsible with the newer power. They started fighting against eachother, so if any other pawn was in its path, they will attack each other.
+Given a N * N grid, and a value P which specifies the number of pawns and followed by P values which specifies the position of those pawns(promoted to bishops). On our special chessboard, two pawns attack each other if they share the same diagonal. This includes pawns that have another pawn located between them, i.e. pawn can attack through pieces.
+How many pair of attacks are possible?
+Input Format
+The first line of input has one integer value N The second line of input has one integer value P, which is the number of Pawns in the board Then it has P lines of inputs which has the indices of each Pawn who were promoted to Bishops.
+Constraints
+1<=N<=10^3 1<=P<=10^9 N - Size of the grid P - Number of Pawns
+Output Format
+One integer Output
+Sample Input 0
+2
+2
+0 0
+1 1
+Sample Output 0
+1
+
