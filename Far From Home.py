db4f11ffeeca0eeb89cd342bc1f02fe472325147
Janani <52209332+Jananipalanivel@users.noreply.github.com>
2020-02-26 18:02:48 +0530

    Update Far From Home.py

--- Far From Home.py
+++ Far From Home.py
@@ -1,3 +1,4 @@
+#question
 In an attempt to reveal the mysteries of human perception, Dr. Vaseegaran thought of conducting a small experiment. His associates will blindfold him and ue an equipment such that he will not be able to hear the surroundig noise. He will be made to walk for a certain distance in a straight line. After a certin point he will be made to turn ninety degerees either to let or right and continues to walk in the same direction and again cotinue to walk , and this continues for N times. After the halt, the blindfold will be removed and Dr. Vaseegaran has to answer how much far is the house is from him. So, How far is he from his home and in which direction?
 Input Format
 One line of spaced inputs, for example 3N - where 3 denotes the distance covered and N represents the direction in which he travells
@@ -10,4 +11,6 @@ Sample Input 0
 Sample Output 0
 5.00SW
 
+************************************************************************************************************************************
+#answer
 
