b6e4dc8c0b7071b8b9e0782f046268559df31608
Janani <52209332+Jananipalanivel@users.noreply.github.com>
2020-02-26 15:30:08 +0530

    Update Binary minimisation.py

--- Binary minimisation.py
+++ Binary minimisation.py
@@ -1,1 +1,14 @@
 
+def swap(n):
+    for i in range(0,len(n)):
+        if n[i]=="1" and n[i+1]=="0":
+            n[i],n[i+1]=n[i+1],n[i]
+            break
+n=input().split()
+rot=int(input())
+for j in range(1,rot+1):
+    if "".join(n).count('10')==0:
+        break
+    swap(n)
+print(n)
+   
