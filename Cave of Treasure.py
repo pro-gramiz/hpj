84395d151886ab8d913f998eae94177a316eb3c7
pooranya <52210652+pooranya@users.noreply.github.com>
2020-03-30 18:37:39 +0530

    Update Cave of Treasure.py

--- Cave of Treasure.py
+++ Cave of Treasure.py
@@ -17,3 +17,18 @@ Sample Output 0
 220
 ************************************************************************************************************************************
 #answer
+def knapSack(W, wt, val, n): 
+    if n == 0 or W == 0 : 
+        return 0
+    if (wt[n-1] > W): 
+        return knapSack(W, wt, val, n-1)
+    else: 
+        return max(val[n-1] + knapSack(W-wt[n-1], wt, val, n-1), 
+                   knapSack(W, wt, val, n-1)) 
+n=int(input())
+wt =list(map(int,input().split())) 
+val = list(map(int,input().split()))
+W = int(input())
+c=knapSack(W, wt, val, n) 
+print(c)
+
